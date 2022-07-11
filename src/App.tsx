2fd83ca0a98f05e2246b4dd5167f39379613f8f1
Mmy26 <yu.b1994masamura@gmail.com>
2022-07-11 11:12:56 +0900

    型の定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "じゃけぇ",
+  hobbies: ["映画", "ゲーム"],
+};
 
 function App() {
+  const [todos, setTodo] = useState<Array<TodoType>>([]);
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodo(res.data);
+      });
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo: TodoType) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
